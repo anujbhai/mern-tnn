b87ecf86008a861d840252e6287c3bacb92c7afe
anuj <anuj@C40LAP-10090.india.rsystems.com>
2022-12-11 21:08:48 +0530

    mern-tnn-06-deleting-data - added dispatch action type for delete workout.

--- frontend/src/context/WorkoutsContext.jsx
+++ frontend/src/context/WorkoutsContext.jsx
@@ -14,6 +14,11 @@ export const workoutsReducer = (state, action) => {
         workouts: [action.payload, ...state.workouts]
       };
 
+    case "DELETE_WORKOUT":
+      return {
+        workouts: state.workouts.filter((w) => w._id !== action.payload._id)
+      };
+
     default:
       return state;
   }
